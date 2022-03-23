477a4c4d966e71cba49e628871ecb499120b18f3
Niek <niekwijsman@yahoo.nl>
2022-03-23 17:23:30 +0100

    base Project.java

--- src/main/java/Project/Project.java
+++ src/main/java/Project/Project.java
@@ -8,16 +8,28 @@ import java.util.ArrayList;
 
 public class Project {
     public Backlog projectBacklog;
-//    public Forum forum
+    //    public Forum forum
     public Account productOwner;
+    public ArrayList<Account> contributors;
     public ArrayList<ISprint> sprints;
     public String name;
 
     public Project(Backlog projectBacklog, Account productOwner, String name) {
         this.projectBacklog = projectBacklog;
         this.productOwner = productOwner;
+        this.contributors = new ArrayList<Account>();
         this.sprints = new ArrayList<ISprint>();
+
         this.name = name;
+    }
+
+    public void enrollProject(Account account){
+        if(!this.contributors.contains(account)){
+            this.contributors.add(account);
+        }else
+        {
+
+        }
     }
 
     public Account getProductOwner() {
