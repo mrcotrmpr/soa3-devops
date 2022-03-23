a98aa06ade8b62e07998513e2b2434770b05f366
Niek <niekwijsman@yahoo.nl>
2022-03-23 17:36:59 +0100

    revert of contributors

--- src/main/java/Project/Project.java
+++ src/main/java/Project/Project.java
@@ -3,7 +3,6 @@ package Project;
 import Account.Account;
 import Backlog.Backlog;
 import Sprint.ISprint;
-import exceptions.ProjectContributersException;
 
 import java.util.ArrayList;
 
@@ -11,26 +10,15 @@ public class Project {
     public Backlog projectBacklog;
     //    public Forum forum
     public Account productOwner;
-    public ArrayList<Account> contributors;
     public ArrayList<ISprint> sprints;
     public String name;
 
     public Project(Backlog projectBacklog, Account productOwner, String name) {
         this.projectBacklog = projectBacklog;
         this.productOwner = productOwner;
-        this.contributors = new ArrayList<Account>();
         this.sprints = new ArrayList<ISprint>();
 
         this.name = name;
-    }
-
-    public void enrollProject(Account account) throws ProjectContributersException {
-        if(!this.contributors.contains(account)){
-            this.contributors.add(account);
-        }else
-        {
-            throw new ProjectContributersException("Contributor already takes part in this project!");
-        }
     }
 
     public Account getProductOwner() {
--- src/main/java/exceptions/ProjectContributersException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class ProjectContributersException extends Exception{
-
-    public ProjectContributersException(String message) {
-        super(message);
-        System.out.println("CONTRIBUTERS EXCEPTION: " + message);
-    }
-}
