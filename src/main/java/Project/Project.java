2d0c9ef79934102559efcc23e863ee50f6e84d9e
Niek <niekwijsman@yahoo.nl>
2022-03-23 17:06:23 +0100

    base Project.java

--- src/main/java/Project/Project.java
+++ src/main/java/Project/Project.java
@@ -1,4 +1,37 @@
 package Project;
 
+import Account.Account;
+import Backlog.Backlog;
+import Sprint.ISprint;
+
+import java.util.ArrayList;
+
 public class Project {
+    public Backlog projectBacklog;
+//    public Forum forum
+    public Account productOwner;
+    public ArrayList<ISprint> sprints;
+    public String name;
+
+    public Project(Backlog projectBacklog, Account productOwner, String name) {
+        this.projectBacklog = projectBacklog;
+        this.productOwner = productOwner;
+        this.sprints = new ArrayList<ISprint>();
+        this.name = name;
+    }
+
+    public Account getProductOwner() {
+        return productOwner;
+    }
+
+    public ArrayList<ISprint> getSprints() {
+        return sprints;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void addSprints(ISprint sprint){
+        this.sprints.add(sprint);
+    }
 }
