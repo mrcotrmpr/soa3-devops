12ee0fd3b2516e43333af81d22fc3a37a8b6b1d3
Niek <niekwijsman@yahoo.nl>
2022-03-25 16:37:43 +0100

    added constructors

--- src/main/java/Account/Account.java
+++ src/main/java/Account/Account.java
@@ -7,5 +7,12 @@ public abstract class Account {
     public String phoneNUmber;
     public String slackUsername;
 
+    public Account(String name, int employeeNumber, String email, String phoneNUmber, String slackUsername) {
+        this.name = name;
+        this.employeeNumber = employeeNumber;
+        this.email = email;
+        this.phoneNUmber = phoneNUmber;
+        this.slackUsername = slackUsername;
+    }
 
 }
--- src/main/java/Account/Developer.java
+++ src/main/java/Account/Developer.java
@@ -1,4 +1,7 @@
 package Account;
 
 public class Developer extends Account{
+    public Developer(String name, int employeeNumber, String email, String phoneNUmber, String slackUsername) {
+        super(name, employeeNumber, email, phoneNUmber, slackUsername);
+    }
 }
