98354e68ac6a5976dcf050a5634b38b15d860129
Niek <niekwijsman@yahoo.nl>
2022-05-05 16:54:37 +0200

    working activities done check in backlogitem

--- src/main/java/Backlog/TestedState.java
+++ src/main/java/Backlog/TestedState.java
@@ -22,7 +22,6 @@ public class TestedState implements IBacklogItemState {
 
     @Override
     public void changeToReadyForTestingState() {
-        //TODO: F13 Als tester wil ik een notificatie krijgen als een backlog item in de fase Ready for testing komt zodat ik weet wanneer ik moet gaan testen
         this._backlogItem.setState(new ReadyForTestingState(this._backlogItem));
     }
 
