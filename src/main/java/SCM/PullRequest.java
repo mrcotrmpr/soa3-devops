acd6c41c685735b627c0e6cc69676a7885300c46
mrcotrmpr <marco1088@hotmail.nl>
2022-06-24 20:10:50 +0200

    Add SCM code

--- src/main/java/SCM/PullRequest.java
+++ src/main/java/SCM/PullRequest.java
@@ -1,5 +1,46 @@
 package SCM;
 
+import java.util.ArrayList;
+
 public class PullRequest {
 
+    private final int id;
+    private int title;
+    private String description;
+    private ArrayList<Commit> commitList;
+
+    public PullRequest(int id, int title, String description, ArrayList<Commit> commitList) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.commitList = commitList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getTitle() {
+        return title;
+    }
+
+    public void setTitle(int title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<Commit> getCommitList() {
+        return commitList;
+    }
+
+    public void setCommitList(ArrayList<Commit> commitList) {
+        this.commitList = commitList;
+    }
 }
