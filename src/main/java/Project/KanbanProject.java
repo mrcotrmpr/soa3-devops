1a8ddb7d3df25ebf608a8487b72c8f2a81e07a3a
mrcotrmpr <marco1088@hotmail.nl>
2022-06-25 14:21:11 +0200

    Add simple project factory

---
+++ src/main/java/Project/KanbanProject.java
@@ -0,0 +1,34 @@
+package Project;
+
+import Account.Account;
+import Backlog.Backlog;
+
+public class KanbanProject implements IProject{
+
+    public String name;
+    public Account productOwner;
+    public Backlog projectBacklog;
+
+    public KanbanProject(String name) {
+        this.name = name;
+        this.projectBacklog = new Backlog();
+    }
+
+    public void setProductOwner(Account productOwner) {
+        this.productOwner = productOwner;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Account getProductOwner() {
+        return this.productOwner;
+    }
+
+    public Backlog getProjectBacklog() {
+        return this.projectBacklog;
+    }
+}
