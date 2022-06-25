a78f88687454ed0e9c2fc2c86be3d5ad65e4eae3
mrcotrmpr <marco1088@hotmail.nl>
2022-06-25 12:44:32 +0200

    Add pipeline and forum tests

--- src/test/java/Forum/ForumTests.java
+++ src/test/java/Forum/ForumTests.java
@@ -1,12 +1,158 @@
 package Forum;
 
+import Backlog.*;
+import Notification.INotifier;
+import Notification.MailNotify;
+import Notification.NotificationService;
+import Notification.Subscriber;
+import exceptions.ChangeBacklogStateException;
+import nl.altindag.console.ConsoleCaptor;
 import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.*;
 
 public class ForumTests {
 
-    @Test
-    public void initialTest(){
+    ConsoleCaptor consoleCaptor = new ConsoleCaptor();
 
+    @Test
+    public void T34_1_backlog_item_is_created_with_empty_thread(){
+        // Arrange
+        BacklogItem backlogItem = new BacklogItem("BacklogItem", 0, 1, 2);
+
+        // Act
+        DiscussionThread thread = backlogItem.getThread();
+
+        // Assert
+        assertNotNull(thread);
+        assertEquals(0, thread.getComments());
+    }
+
+    @Test
+    public void T35_1_can_respond_to_discussion_thread(){
+        // Arrange
+        BacklogItem backlogItem = new BacklogItem("BacklogItem", 0, 1, 2);
+
+        // Act
+        DiscussionThread thread = backlogItem.getThread();
+        int initialComments = thread.getComments();
+        thread.addComment("new comment");
+        int newComments = thread.getComments();
+
+        // Assert
+        assertEquals(0, initialComments);
+        assertEquals(1, newComments);
+    }
+
+    @Test
+    public void T35_2_can_respond_to_discussion_thread_when_active(){
+        // Arrange
+        BacklogItem backlogItem = new BacklogItem("BacklogItem", 0, 1, 2);
+
+        // Act
+        DiscussionThread thread = backlogItem.getThread();
+        int initialComments = thread.getComments();
+        boolean active = thread.isActive();
+        thread.addComment("new comment");
+        int newComments = thread.getComments();
+
+        // Assert
+        assertEquals(0, initialComments);
+        assertTrue(active);
+        assertEquals(1, newComments);
+    }
+
+    @Test
+    public void T35_3_can_not_respond_to_discussion_thread_when_inactive(){
+        // Arrange
+        BacklogItem backlogItem = new BacklogItem("BacklogItem", 0, 1, 2);
+
+        // Act
+        DiscussionThread thread = backlogItem.getThread();
+        int initialComments = thread.getComments();
+
+        thread.setActive(false);
+        boolean active = thread.isActive();
+
+        thread.addComment("new comment");
+        int newComments = thread.getComments();
+
+        // Assert
+        assertEquals(0, initialComments);
+        assertFalse(active);
+        assertEquals(0, newComments);
+    }
+
+    @Test
+    public void T36_1_subscribers_of_discussion_thread_receive_notification(){
+        // Arrange
+        BacklogItem backlogItem = new BacklogItem("BacklogItem", 0, 1, 2);
+        DiscussionThread thread = backlogItem.getThread();
+
+        INotifier notifier = new MailNotify();
+        Subscriber sub = new NotificationService(notifier);
+        thread.subscribe(sub);
+
+        // Act
+        thread.addComment("new comment");
+
+        // Assert
+        assert(consoleCaptor.getStandardOutput()).contains("Sending mail: Received a new comment; new comment");
+        consoleCaptor.clearOutput();
+    }
+
+    @Test
+    public void T37_38_1_thread_status_should_be_true_when_status_todo(){
+        // Arrange
+        BacklogItem backlogItem = new BacklogItem("BacklogItem", 0, 1, 2);
+
+        // Act
+        IBacklogItemState state = backlogItem.getState();
+
+        DiscussionThread thread = backlogItem.getThread();
+        boolean threadState = thread.isActive();
+
+        // Assert
+        assertEquals(ToDoState.class, state.getClass());
+        assertTrue(threadState);
+    }
+
+    @Test
+    public void T37_38_2_thread_status_should_be_true_when_status_in_progress() throws ChangeBacklogStateException {
+        // Arrange
+        BacklogItem backlogItem = new BacklogItem("BacklogItem", 0, 1, 2);
+
+        // Act
+        backlogItem.getState().changeToDoingState();
+        IBacklogItemState state = backlogItem.getState();
+
+        DiscussionThread thread = backlogItem.getThread();
+        boolean threadState = thread.isActive();
+
+        // Assert
+        assertEquals(DoingState.class, state.getClass());
+        assertTrue(threadState);
+    }
+
+    @Test
+    public void T37_38_2_thread_status_should_be_false_when_status_done() throws ChangeBacklogStateException {
+        // Arrange
+        BacklogItem backlogItem = new BacklogItem("BacklogItem", 0, 1, 2);
+
+        // Act
+        backlogItem.getState().changeToDoingState();
+        backlogItem.getState().changeToReadyForTestingState();
+        backlogItem.getState().changeToTestingState();
+        backlogItem.getState().changeToTestedState();
+        backlogItem.getState().changeToDoneState();
+
+        IBacklogItemState state = backlogItem.getState();
+        DiscussionThread thread = backlogItem.getThread();
+        boolean threadState = thread.isActive();
+
+        // Assert
+        assertEquals(DoneState.class, state.getClass());
+        assertFalse(threadState);
     }
 
 }
--- src/test/java/Pipeline/PipelineTests.java
+++ src/test/java/Pipeline/PipelineTests.java
@@ -1,12 +1,48 @@
 package Pipeline;
 
+import PipeLine.PipeLine;
 import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.AssertJUnit.assertFalse;
 
 public class PipelineTests {
 
     @Test
-    public void initialTest(){
+    public void T_22_1_pipeline_is_created_successfully(){
+        // Arrange
+        PipeLine pipeLine = new PipeLine("New Pipeline", false);
 
+        // Act
+        String name = pipeLine.getPipeLineName();
+
+        // Assert
+        assertEquals(name, "New Pipeline");
+    }
+
+    @Test
+    public void T_22_2_automatic_pipeline_is_created_successfully(){
+        // Arrange
+        PipeLine pipeLine = new PipeLine("New Pipeline", true);
+
+        // Act
+        boolean automatic = pipeLine.isAutomatic();
+
+        // Assert
+        assertTrue(automatic);
+    }
+
+    @Test
+    public void T_22_3_manual_pipeline_is_created_successfully(){
+        // Arrange
+        PipeLine pipeLine = new PipeLine("New Pipeline", false);
+
+        // Act
+        boolean automatic = pipeLine.isAutomatic();
+
+        // Assert
+        assertFalse(automatic);
     }
 
 }
