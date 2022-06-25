2ff25c6a355591f5394cfebaf1f43b50d27d2baa
mrcotrmpr <marco1088@hotmail.nl>
2022-06-25 11:44:01 +0200

    Add notification tests

--- src/test/java/Notification/NotificationTests.java
+++ src/test/java/Notification/NotificationTests.java
@@ -1,12 +1,36 @@
 package Notification;
 
+import nl.altindag.console.ConsoleCaptor;
 import org.testng.annotations.Test;
 
 public class NotificationTests {
 
-    @Test
-    public void initialTest(){
+    ConsoleCaptor consoleCaptor = new ConsoleCaptor();
 
+    @Test
+    public void T44_1_send_notification_via_slack() {
+        // Arrange
+        INotifier notifier = new SlackNotify();
+
+        // Act
+        notifier.sendNotification("This is a slack message");
+
+        // Assert
+        assert(consoleCaptor.getStandardOutput()).contains("Sent Slack message: This is a slack message");
+        consoleCaptor.clearOutput();
+    }
+
+    @Test
+    public void T44_2_send_notification_via_mail() {
+        // Arrange
+        INotifier notifier = new MailNotify();
+
+        // Act
+        notifier.sendNotification("This is a mail");
+
+        // Assert
+        assert(consoleCaptor.getStandardOutput()).contains("Sending mail: This is a mail");
+        consoleCaptor.clearOutput();
     }
 
 }
